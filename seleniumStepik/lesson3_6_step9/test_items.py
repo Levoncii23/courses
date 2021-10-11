11d9324670b5e38d44d58a966643014dcee49b71
levoncii23 <levoncii23@gmail.com>
2021-10-11 11:45:37 +0300

    upload lesson3_6_step9

---
+++ seleniumStepik/lesson3_6_step9/test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_basket_btn(browser):
+    browser.get(link)
+    # time.sleep(20)
+    assert len(browser.find_elements_by_class_name("btn-add-to-basket")) > 0, "Basket not found!"
